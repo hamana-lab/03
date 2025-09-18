f0633e4f29c3fbcb8a00d190acee8e6d9081b3fe
hamana-lab <hamana@sookmyung.ac.kr>
2025-09-18 16:22:45 +0900

    week03_3

--- main.c
+++ main.c
@@ -2,16 +2,19 @@
 #include <stdlib.h>
 
 int main(void){
-  char c;
-  int i;
+    char c,d;
+    int i,j;
     
-  printf("input a number :");
-  scanf("%c", &c);
-  
-  i = c-'0';
-  printf("The input numer is %i\n", i);
-  
-  system("PAUSE");
-  return 0;
+    printf("enter a character :");
+    scanf("%c", &c);
+    
+    i = c;
+    j = c+1;
+    d = c+1;
+    
+    printf("the next character of %c (%i) is %c (%i)\n",c,i,d,j);
+    
+    system("PAUSE");
+    return 0;
   
 }
